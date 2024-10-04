1955b00761248e5cf96cab549ffaa07a9d78cd51
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-10-04 13:53:57 +0200

    udpates

--- notebooks/quizzes.py
+++ notebooks/quizzes.py
@@ -1,25 +1,26 @@
 from jupyterquiz import display_quiz
 
-questions = [
-    {
-        "question": r"""Does the `git merge other_branch` command always create a merge conflict?""",
-        "type": "multiple_choice",
-        "answers": [
-            {
-                "answer": "yes",
-                "correct": False
-            },
-            {
-                "answer": "no",
-                "correct": True,
-                "feedback": r"""
+QUESTIONS = {
+    "merge_conflict_quizz": [
+        {
+            "question": r"""Does the `git merge other_branch` command always create a merge conflict?""",
+            "type": "multiple_choice",
+            "answers": [
+                {"answer": "yes", "correct": False},
+                {
+                    "answer": "no",
+                    "correct": True,
+                    "feedback": r"""
                     If the branches do not change the same part of the codebase, Git will not create a merge conflict.
-                """
-            }
-        ]
-    },
-]
+                """,
+                },
+            ],
+        },
+    ],
+}
+
 
 def run(selection):
-    if selection == 1:
-        display_quiz(questions, shuffle_answers=False)
+    if selection not in QUESTIONS:
+        return
+    display_quiz(QUESTIONS[selection], shuffle_answers=False)
